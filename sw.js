41b32028a074782d3398a527ea0754d5d77c1343
delic-psy <yabaipark@gmail.com>
2026-09-09 03:39:38 +0900

    Update cache version to wage-delivery-pwa-v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE="wage-delivery-pwa-v1";
+const CACHE="wage-delivery-pwa-v2";
 const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
